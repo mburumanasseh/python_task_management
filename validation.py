37bf84d93d7ab1fa8c9dce2bf544a8d5cc72c8c4
mburumanasseh <manassehmugo714@gmail.com>
2026-06-04 22:05:29 +0300

    Add validation.py file

---
+++ validation.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def validate_task_title(title):
+    if not isinstance(title, str):
+        return False
+    if title.strip() == "":
+        return False
+    return True
+
+def validate_task_description(description):
+    if not isinstance(description, str):
+        return False
+    if description.strip() == "":
+        return False
+    return True
+
+def validate_due_date(due_date):
+    try:
+        datetime.strptime(due_date, "%Y-%m-%d")
+        return True
+    except ValueError:
+        return False
